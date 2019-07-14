c422c6661cf18617b9ab7874714d31a839d535b8
Adam Nemecek <adamnemecek@gmail.com>
2019-07-14 13:56:40 -0700

    stuff

--- src/DualFourier.jl
+++ src/DualFourier.jl
@@ -1,10 +1,11 @@
 module DualFourier
 
-using Signal: slow_cconv
+
 using DualNumbers
 using FFTW: fft, ifft
 
 include("fft.jl")
+
 
 export dual_fft
 
--- test/fft.jl
+++ test/fft.jl
@@ -1,6 +1,12 @@
 
 module TestFFT
+using FFTW: fft
+using Signal: slow_cconv
+using DualFourier: dual_fft
+
 @testset "convolution theorem" begin
-    
+    a = rand(ComplexF64)
+    b = rand(ComplexF64)
+    @test fft(a) .* fft(b) ≈ fft(slow_cconv(a,b))
 end
 end
