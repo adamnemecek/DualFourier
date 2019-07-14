0643f2fc3f22ca99ad813b641c144e4b5db13444
Adam Nemecek <adamnemecek@gmail.com>
2019-07-14 13:59:16 -0700

    convolution works

--- test/fft.jl
+++ test/fft.jl
@@ -3,10 +3,11 @@ module TestFFT
 using FFTW: fft
 using Signal: slow_cconv
 using DualFourier: dual_fft
+using Test: @test, @testset
 
 @testset "convolution theorem" begin
-    a = rand(ComplexF64)
-    b = rand(ComplexF64)
-    @test fft(a) .* fft(b) ≈ fft(slow_cconv(a,b))
+    a = rand(ComplexF64, 10)
+    b = rand(ComplexF64, 10)
+    @test fft(a) .* fft(b) ≈ fft(slow_cconv(a,b, length(a)))
 end
 end
